2de7b29eaf822034fa63d6a64d724e1e92eaf179
Akash3121 <akashreddyjammula@gmail.com>
2024-08-20 11:39:03 -0500

    Code Updation

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ def chat_with_gpt(prompt):
     )
     return response.choices[0].message.content.strip()
 
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["bye", "break", "exit", "quit"]:
+            break
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
 
